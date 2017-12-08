866a419b601d66f26ef09dbdd5c67ea3aa8acfc2
vagrant <vagrant@homestead>
2017-12-08 15:21:55 +0000

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function welcome()
+    {
+    	//$products = Product::paginate(9);
+    	return view('welcome');
+    }
+}
